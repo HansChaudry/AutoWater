3297d96040cf9289c90f9a7cfb78a78eed0689da
Hans Chaudry <hanschaudry@gmail.com>
2024-12-25 10:30:57 -0500

    Added PumpState custom library

---
+++ lib/PumpState/src/PumpState.h
@@ -0,0 +1,10 @@
+#ifndef PUMP_STATE
+#define PUMP_STATE
+
+enum PumpState {
+    Ready,
+    Busy,
+    Empty
+};
+
+#endif 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
+#include <PumpState.h>
 
 // put function declarations here:
 int pumpPin = 7;
-bool pumpState = false;
+PumpState pumpState;
 void startPump();
 void stopPump();
 
@@ -10,6 +11,7 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(pumpPin, OUTPUT);
+  pumpState = Ready;
 }
 
 void loop() {
@@ -17,28 +19,29 @@ void loop() {
   int val;
   val=analogRead(0);     // sensor Aout pin connected to A0 pin of Ardiuno
   Serial.println(val);        // print the value in serial monitor
-  if(val > 400){
-    if (!pumpState)
-    {
-      startPump();
-    }
-  }
   
-  if(val < 400){
-    if(pumpState){
-      stopPump();
-    }
+  if(val > 350){
+    startPump();
   }
+  else{
+    stopPump();
+  }
+
   delay(1000);
 }
 
 // put function definitions here:
 void startPump() {
-  digitalWrite(pumpPin, HIGH);
-  pumpState = true;
+  if(!pumpState){
+    digitalWrite(pumpPin, HIGH);
+    pumpState = Busy;
+  }
+
+  //TODO: check water level. If the level is too low stop the pump. and set pump state to empty
+
 }
 
 void stopPump(){
   digitalWrite(pumpPin, LOW);
-  pumpState = false;
+  pumpState = Ready;
 }
